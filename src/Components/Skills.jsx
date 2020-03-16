f96e9a5f3392e42f5e9325379cb699ea3417f24a
ravindra.maddukuri <ravindra.m@stellentsoft.com>
2020-03-16 18:19:46 +0530

    Second push

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -129,6 +129,101 @@ class Skills extends Component {
                   </div>
                 </div>
               </div>
+              <div className="col-md-6">
+                <div className="progress-container progress-primary">
+                  <span className="progress-badge">REDUX</span>
+                  <div className="progress">
+                    <div
+                      className="progress-bar progress-bar-primary"
+                      data-aos="progress-full"
+                      data-aos-offset="10"
+                      data-aos-duration="2000"
+                      role="progressbar"
+                      aria-valuenow="60"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "75%" }}
+                    ></div>
+                    <span className="progress-value">75%</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="progress-container progress-primary">
+                  <span className="progress-badge">FIREBASE</span>
+                  <div className="progress">
+                    <div
+                      className="progress-bar progress-bar-primary"
+                      data-aos="progress-full"
+                      data-aos-offset="10"
+                      data-aos-duration="2000"
+                      role="progressbar"
+                      aria-valuenow="60"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "75%" }}
+                    ></div>
+                    <span className="progress-value">75%</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="progress-container progress-primary">
+                  <span className="progress-badge">AIRTABLE DATABASE</span>
+                  <div className="progress">
+                    <div
+                      className="progress-bar progress-bar-primary"
+                      data-aos="progress-full"
+                      data-aos-offset="10"
+                      data-aos-duration="2000"
+                      role="progressbar"
+                      aria-valuenow="60"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "75%" }}
+                    ></div>
+                    <span className="progress-value">65%</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="progress-container progress-primary">
+                  <span className="progress-badge">TypeScript</span>
+                  <div className="progress">
+                    <div
+                      className="progress-bar progress-bar-primary"
+                      data-aos="progress-full"
+                      data-aos-offset="10"
+                      data-aos-duration="2000"
+                      role="progressbar"
+                      aria-valuenow="60"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "75%" }}
+                    ></div>
+                    <span className="progress-value">70%</span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="progress-container progress-primary">
+                  <span className="progress-badge">Hooks</span>
+                  <div className="progress">
+                    <div
+                      className="progress-bar progress-bar-primary"
+                      data-aos="progress-full"
+                      data-aos-offset="10"
+                      data-aos-duration="2000"
+                      role="progressbar"
+                      aria-valuenow="60"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                      style={{ width: "75%" }}
+                    ></div>
+                    <span className="progress-value">80%</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
